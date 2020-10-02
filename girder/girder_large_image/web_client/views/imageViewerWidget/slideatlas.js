6aff1a2f450a5cbb22ed7457acbf828ea72be09e
Charles law <charles.law@kitware.com>
2020-10-02 12:11:03 -0400

    Support new slide atlas viewer features

--- girder/girder_large_image/web_client/views/imageViewerWidget/slideatlas.js
+++ girder/girder_large_image/web_client/views/imageViewerWidget/slideatlas.js
@@ -30,7 +30,6 @@ var SlideAtlasImageViewerWidget = ImageViewerWidget.extend({
     },
 
     render: function () {
-        var errmsg;
         // If script or metadata isn't loaded, then abort
         if (!window.SA || !this.tileWidth || !this.tileHeight || this.deleted) {
             return this;
@@ -38,19 +37,6 @@ var SlideAtlasImageViewerWidget = ImageViewerWidget.extend({
 
         if (this.viewer) {
             // don't rerender the viewer
-            return this;
-        }
-
-        if (this.tileWidth !== this.tileHeight) {
-            errmsg = 'The SlideAtlas viewer only supports square tiles.';
-        }
-        if (this.tileWidth > 256) {
-            errmsg = 'The SlideAtlas viewer does not support tiles wider than 256 pixels.';
-        }
-        if (errmsg) {
-            this.viewer = $('<div/>').text(errmsg);
-            this.$el.append(this.viewer);
-            console.error(errmsg);
             return this;
         }
 
@@ -62,23 +48,71 @@ var SlideAtlasImageViewerWidget = ImageViewerWidget.extend({
             root = __webpack_public_path__ || root; // eslint-disable-line
         } catch (err) { }
         root = root.replace(/\/$/, '');
-        window.$(this.el).saViewer({
+
+        var tileSource = {
+            height: this.sizeY,
+            width: this.sizeX,
+            tileWidth: this.tileWidth,
+            tileHeight: this.tileHeight,
+            minLevel: 0,
+            maxLevel: this.levels - 1,
+            units: 'mm',
+            spacing: [this.mm_x, this.mm_y],
+            getTileUrl: (level, x, y, z) => {
+                // Drop the "z" argument
+                return this._getTileUrl(level, x, y);
+            }
+        };
+        if (!this.mm_x) {
+            // tileSource.units = 'pixels';
+            tileSource.spacing = [1, 1];
+        }
+
+
+	window.SA.SAViewer(window.$(this.el), {
             zoomWidget: true,
             drawWidget: true,
             prefixUrl: root + '/plugins/large_image/extra/slideatlas/img/',
-            tileSource: {
-                height: this.sizeY,
-                width: this.sizeX,
-                tileSize: this.tileWidth,
-                minLevel: 0,
-                maxLevel: this.levels - 1,
-                getTileUrl: (level, x, y, z) => {
-                    // Drop the "z" argument
-                    return this._getTileUrl(level, x, y);
-                }
-            }
+            tileSource: tileSource
         });
         this.viewer = this.el.saViewer;
+
+	// Create the annotation GUI
+        this.girderGui = new window.SAM.LayerPanel(this.viewer, this.itemId);
+        $(this.el).css({position: 'relative'});
+	// Create the full screen button
+        window.SA.SAFullScreenButton($(this.el))
+          .css({'position': 'absolute', 'left': '2px', 'top': '2px'});
+        SA.GirderView = this;
+
+        // Set the view from the URL if bounds are specified.
+        var curRoute = Backbone.history.fragment,
+            routeParts = splitRoute(curRoute),
+            queryString = parseQueryString(routeParts.name);
+        if (queryString.bounds) {
+            var rot = 0
+            if (queryString.rotate) {
+		rot = parseInt(queryString.rotate);
+            }
+            var bds = queryString.bounds.split(',')
+            var x0 = parseInt(bds[0])
+            var y0 = parseInt(bds[1])
+            var x1 = parseInt(bds[2])
+            var y1 = parseInt(bds[3])
+            this.viewer.SetCamera([(x0+x1)*0.5, (y0+y1)*0.5], rot, (y1-y0));
+        } else if ('camera' in this.model.attributes.meta) {
+            var cam = this.model.attributes.meta.camera;
+            if (! 'rotation' in cam) {
+		cam.rotation = 0;
+            }
+            this.viewer.SetCamera(cam.center, cam.rotation, cam.height);      
+        }
+	// This facilitates aligning images in a folder as a stack.
+        if ('trans' in this.model.attributes.meta) {
+            var trans = this.model.attributes.meta.camera;
+            var cam = this.viewer.GetCamera();
+            cam.SetImageToWorldTransform(trans);
+        }
 
         this.trigger('g:imageRendered', this);
         return this;
